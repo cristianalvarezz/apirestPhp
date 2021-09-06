5a314acccac6023baf419220286e6daf2e18a7f1
cristianalvarezz <alvarescristian46@gmail.com>
2021-09-06 12:03:01 -0500

    Crud completo

--- auth.php
+++ auth.php
@@ -20,12 +20,15 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
     //delvolvemos una respuesta
     header('Content-Type: application/json');
+    //el reult eesta en la clase de respuesta 
     if(isset($datosArray["result"]["error_id"])){
+    
         $responseCode = $datosArray["result"]["error_id"];
         http_response_code($responseCode);
     }else{
         http_response_code(200);
     }
+    //al codificarlo como json lo estamos volviendo un string 
     echo json_encode($datosArray);
 
 
